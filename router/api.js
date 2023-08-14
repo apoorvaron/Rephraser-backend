1229909244ab5c4e5e6f1130658543c420e4db8d
apoorvaron <apoorv.aron02@gmail.com>
2023-08-14 18:46:08 +0530

    Registration API that handles Missing Credentials

--- router/api.js
+++ router/api.js
@@ -3,8 +3,10 @@ const router = Router();
 
 /** import all controllers */
 const controllerLogin = require("../controllers/login.js");
+const controllerRegister = require("../controllers/register.js");
 
 /** POST Methods */
 router.route("/login").post(controllerLogin.login); // login in app
+router.route("/register").post(controllerRegister.register); // login in app
 
 module.exports = router;
