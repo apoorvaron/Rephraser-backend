3a2abe9cb09ab67f674634620171c3ee0f9656a6
apoorvaron <apoorv.aron02@gmail.com>
2023-08-14 18:58:16 +0530

    Registration API that handles already registered username

--- controllers/register.js
+++ controllers/register.js
@@ -16,6 +16,11 @@ async function register(req, res) {
       return res.status(400).json({ message: 'Missing Credentials' });
     }
 
+    // Check if username is already registered
+    const userCheckResult = await db.query('SELECT * FROM users WHERE username = $1', [username]);
+    if (userCheckResult.rows.length > 0) {
+      return res.status(400).json({ message: 'Username already registered' });
+    }
   } catch (error) {
     console.error('Error during registration:', error);
     res.status(500).json({ message: 'Internal server error' });
