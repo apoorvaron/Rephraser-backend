846483bafed929f1d010af8c5a61c39ac536d45f
apoorvaron <apoorv.aron02@gmail.com>
2023-08-14 14:11:13 +0530

    Update the type of id column

--- migrations/20230810135735-create-user-table.js
+++ migrations/20230810135735-create-user-table.js
@@ -1,12 +1,11 @@
 exports.up = async function(db) {
-  // console.log(db);
   await db.createTable('users', {
-  id: { type: 'string', primaryKey: true },
-  username: { type: 'string', notNull: true },
-  password: { type: 'string', notNull: true },
-  created_at: { type: 'timestamp', notNull: true },
-  updated_at: { type: 'timestamp', notNull: true },
-});
+    id: { type: 'serial', primaryKey: true },
+    username: { type: 'string', notNull: true },
+    password: { type: 'string', notNull: true },
+    created_at: { type: 'timestamp', notNull: true },
+    updated_at: { type: 'timestamp', notNull: true },
+  });
 };
 
 exports.down = async function(db) {
