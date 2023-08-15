70577533a4ca9f59312ad5a47c2e566e14d858b0
Pranav Tiwari <pranav@tiwari@checkhq.com>
2023-08-15 23:58:25 +0530

    Changes db connection to DB URL

--- config/db.js
+++ config/db.js
@@ -2,13 +2,9 @@ const { Client } = require("pg");
 const env = require("dotenv");
 env.config();
 
-const db = new Client({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
-});
+const connectionString = process.env.DB_URL
+
+const db = new Client({connectionString});
 
 db.connect();
 
