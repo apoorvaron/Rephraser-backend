62c4e65ff694401659f8e984dd84c89d430217c3
nidhi-rathore <nidhirathore7022@gmail.com>
2023-08-18 15:01:03 +0530

    Revert "Docstrings added for login and registration APIs"
    This reverts commit 042f13941167e5cb82da33f741e7d7dbda2f29ee.

--- controllers/login.js
+++ controllers/login.js
@@ -1,14 +1,3 @@
-/**
- * Log in an existing user.
- *
- * This API endpoint allows users to log in by providing their username and password.
- * It verifies the provided credentials and returns a JWT token upon successful login.
- *
- * @param {object} req - The HTTP request object.
- * @param {object} res - The HTTP response object.
- * @returns {object} The HTTP response containing login status and token.
- */
-
 const bcrypt = require('bcrypt');
 const db = require('../config/db.js');
 const { generateToken } = require('../utils/jwtUtils');
@@ -16,28 +5,18 @@ require('dotenv').config();
 async function login(req, res) {
     try {
         const { username, password } = req.body;
-
-        // Check for missing credentials
         if (username === undefined || username === "" || password === undefined || password === "") {
             return res.status(400).json({ message: 'Missing Credentials' });
         }
-
-        // Query the database for the user
         const result = await db.query('SELECT * FROM users WHERE username = $1', [username]);
         const user = result.rows[0];
-
-        // Check if the user exists
         if (!user) {
             return res.status(400).json({ message: 'User does not exist' });
         }
-
-        // Compare passwords
         const passwordMatch = await bcrypt.compare(password, user.password);
         if (!passwordMatch) {
             return res.status(400).json({ message: 'Wrong Password' });
         }
-
-        // Generate a JWT token using the utility function
         const token = generateToken(user.id); // Generate token using the utility function
         return res.status(200).json({ message: 'Login successful', token: token });
     } catch (error) {
