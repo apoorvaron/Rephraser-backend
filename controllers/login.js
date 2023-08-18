b7f270e4f34b71349a4049c5b242230e9132b822
Nidhi Rathore <nidhirathore7022@gmail.com>
2023-08-19 00:23:48 +0530

    Update login.js

--- controllers/login.js
+++ controllers/login.js
@@ -1,28 +1,50 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt'); // Import bcrypt
 const db = require('../config/db.js');
+const env = require("dotenv");
+env.config();
 const { generateToken } = require('../utils/jwtUtils');
-require('dotenv').config();
 
+
+/** POST: http://localhost:3000/api/login 
+ * @param: {
+  "username" : "example123",
+  "password" : "admin123"
+}
+*/
 async function login(req, res) {
     try {
+
         const { username, password } = req.body;
-        if (username === undefined || username === "" || password === undefined || password === "") {
+
+        // Check Username/Password are present or not 
+        if(username===undefined || username==="" || password===undefined || password===""){
             return res.status(400).json({ message: 'Missing Credentials' });
         }
+    
+        // Retrieve the user based on the username
         const result = await db.query('SELECT * FROM users WHERE username = $1', [username]);
         const user = result.rows[0];
+    
+        // Check if the user exists
         if (!user) {
-            return res.status(400).json({ message: 'User does not exist' });
+          return res.status(400).json({ message: 'User does not exist' });
         }
+    
+        // Compare the provided password with the hashed password stored in the database
         const passwordMatch = await bcrypt.compare(password, user.password);
+    
         if (!passwordMatch) {
-            return res.status(400).json({ message: 'Wrong Password' });
+          return res.status(400).json({ message: 'Wrong Password' });
         }
+    
+        // Create a JWT token
         const token = generateToken(user.id); // Generate token using the utility function
         return res.status(200).json({ message: 'Login successful', token: token });
-    } catch (error) {
+   
+      } catch (error) {
         console.error('Error during login:', error);
         res.status(500).json({ message: 'Internal server error' });
-    }
+      }
 }
+
 module.exports = { login };
