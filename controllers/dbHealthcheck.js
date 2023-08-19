8db0137020b7c48417d82b5ea23a17b81c4dfd9e
apoorvaron <apoorv.aron02@gmail.com>
2023-08-19 16:43:38 +0530

    Connect get API /config/dbHealthCheck with dbUtils.js instead of db.js

--- controllers/dbHealthcheck.js
+++ controllers/dbHealthcheck.js
@@ -1,15 +1,18 @@
-const db = require("../config/db.js");
+const DBUtils = require('../utils/dbUtils.js'); // Import the DBUtils module
 
 async function dbHealthCheck(req, res) {
     console.log("Checking database health...");
+    const dbUtils = new DBUtils(); 
+
     try {
-        await db.query('SELECT 1');
+        // Connect to the database using DBUtils
+        await dbUtils.run('SELECT 1'); 
         console.log('Database is connected');
         res.status(200).send('Database is connected');
     } catch (err) {
         console.log('Database connection error');
         res.status(500).send('Database connection error');
-    }
+    } 
 }
 
-module.exports = { dbHealthCheck }; // Export the function as an object property
+module.exports = { dbHealthCheck }; 
