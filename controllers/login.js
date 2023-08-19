dde3de40e9405c56c9da5e37e64420eb7d9fc21e
apoorvaron <apoorv.aron02@gmail.com>
2023-08-19 13:29:23 +0530

    revert comments

--- controllers/login.js
+++ controllers/login.js
@@ -4,10 +4,18 @@ env.config();
 const { generateToken } = require('../utils/jwtUtils');
 const DBUtils = require('../utils/dbUtils.js');
 
+
+/** POST: http://localhost:3000/api/login 
+ * @param: {
+  "username" : "example123",
+  "password" : "admin123"
+}
+*/
 async function login(req, res) {
   try {
     const { username, password } = req.body;
 
+    // Check Username/Password are present or not
     if (username === undefined || username === "" || password === undefined || password === "") {
       return res.status(400).json({ message: 'Missing Credentials' });
     }
@@ -17,19 +25,23 @@ async function login(req, res) {
     const values = [username];
 
     try {
+      // Retrieve the user based on the username
       const result = await dbUtils.run(query, values);
       const user = result.rows[0];
-
+      
+      // Check if the user exists
       if (!user) {
         return res.status(400).json({ message: 'User does not exist' });
       }
-
+      
+      // Compare the provided password with the hashed password stored in the database
       const passwordMatch = await bcrypt.compare(password, user.password);
 
       if (!passwordMatch) {
         return res.status(400).json({ message: 'Wrong Password' });
       }
-
+      
+      // Create a JWT token
       const token = generateToken(user.id);
       return res.status(200).json({ message: 'Login successful', token: token });
     } catch (error) {
