0649d31bc0cc01724e1ce04e17523891408d4c53
nidhi-rathore <nidhirathore7022@gmail.com>
2023-08-22 22:54:51 +0530

    Fetchin data from correction table according to user_id using jwtg

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+const DBUtils = require('../utils/dbUtils.js');
 const OpenAI = require("openai");
 const env = require("dotenv");
 env.config();
@@ -48,9 +50,33 @@ async function sendChat(req, res) {
 
 async function chatHistory(req, res) {
   try {
-    res.status(200).json(dummyData);
+    const token = req.header('Authorization').replace('Bearer ', '');
+    if (!token) {
+      return res.status(401).json({ message: 'Missing token' });
+    }
+    jwt.verify(token, process.env.JWT_SECRET, async (error, decodedToken) => {
+      if (error) {
+        console.log(error);
+        return res.status(403).json({ message: 'Invalid token' });
+      }
+      const userId = decodedToken.userId;
+      // Use the dbUtils for database connection
+      const dbUtils = new DBUtils();
+      const query = 'SELECT * FROM corrections WHERE user_id = $1';
+      const values = [userId];
+      try {
+        // Retrieve chat history based on the user ID
+        const result = await dbUtils.run(query, values);
+        const chatHistory = result.rows;
+        res.status(200).json({ chatHistory });
+      } catch (error) {
+        console.error('Error retrieving chat history:', error);
+        res.status(500).json({ message: 'Internal server error' });
+      }
+    });
   } catch (error) {
-    res.status(500).send("Internal Server Error");
+    console.error('Error in chatHistory:', error);
+    res.status(500).send('Internal Server Error');
   }
 }
 // Export the functions
