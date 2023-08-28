ff4c7ebab209f77bdd041b1fb737fc65ccab55dd
apoorvaron <apoorv.aron02@gmail.com>
2023-08-28 14:55:54 +0530

    Remove try catch & utlisie error middle ware for register method

--- controllers/register.js
+++ controllers/register.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt'); // Import bcrypt
+const bcrypt = require('bcrypt');
 const { generateToken } = require('../utils/jwtUtils');
 const DBUtils = require('../utils/dbUtils.js');
 const env = require("dotenv");
@@ -11,59 +11,53 @@ env.config();
  }
  */
 async function register(req, res) {
-  try {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    // Check if any input is missing
-    if (!username || !password) {
-      return res.status(400).json({ message: 'Missing Credentials' });
+  // Check if any input is missing
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Missing Credentials' });
+  }
+
+  try {
+    const dbUtils = new DBUtils(); // Create a new instance of DBUtils for each request
+
+    const queryCheckUser = 'SELECT * FROM users WHERE username = $1';
+    const valuesCheckUser = [username];
+
+    // Check if username is already registered
+    const userCheckResult = await dbUtils.run(queryCheckUser, valuesCheckUser);
+    if (userCheckResult.rows.length > 0) {
+      return res.status(400).json({ message: 'Username already registered' });
     }
 
-    try {
-      const dbUtils = new DBUtils(); // Create a new instance of DBUtils for each request
-      
-      const queryCheckUser = 'SELECT * FROM users WHERE username = $1';
-      const valuesCheckUser = [username];
+    // Password validation (min 8 characters, 1 small, 1 capital, and 1 number)
+    const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
-      // Check if username is already registered
-      const userCheckResult = await dbUtils.run(queryCheckUser, valuesCheckUser);
-      if (userCheckResult.rows.length > 0) {
-        return res.status(400).json({ message: 'Username already registered' });
-      }
+    if (!passwordPattern.test(password)) {
+      return res.status(400).json({ message: 'Password is not strong enough' });
+    }
 
-      // Password validation (min 8 characters, 1 small, 1 capital, and 1 number)
-      const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-      if (!passwordPattern.test(password)) {
-        return res.status(400).json({ message: 'Password is not strong enough' });
-      }
+    // Insert new user into the database
+    const insertionResult = await dbUtils.run(
+      'INSERT INTO users (username, password, created_at, updated_at) VALUES ($1, $2, NOW(), NOW()) RETURNING id',
+      [username, hashedPassword]
+    );
 
-      // Hash the password
-      const hashedPassword = await bcrypt.hash(password, 10);
+    if (insertionResult.rows.length > 0) {
+      // Generate token using the utility function
+      const userId = insertionResult.rows[0].id;
+      const token = generateToken(userId);
 
-      // Insert new user into the database
-      const insertionResult = await dbUtils.run(
-        'INSERT INTO users (username, password, created_at, updated_at) VALUES ($1, $2, NOW(), NOW()) RETURNING id',
-        [username, hashedPassword]
-      );       
-
-      if (insertionResult.rows.length > 0) {
-        // Generate token using the utility function
-        const userId = insertionResult.rows[0].id;
-        const token = generateToken(userId);
-
-        return res.status(200).json({ message: 'Registration Successful', token: token });
-      } else {
-        return res.status(500).json({ message: 'Failed to register user' });
-      }
-
-    } catch (error) {
-      console.error('Error during registration:', error);
-      res.status(500).json({ message: 'Internal server error' });
+      return res.status(200).json({ message: 'Registration Successful', token: token });
+    } else {
+      return res.status(500).json({ message: 'Failed to register user' });
     }
   } catch (error) {
-    console.error('Error during registration:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    // Pass the error to the error middleware
+    throw(error);
   }
 }
 
