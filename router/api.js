244cf255d2975b6c9b4386441033a5d63c183866
nidhi-rathore <nidhirathore7022@gmail.com>
2023-09-04 16:12:37 +0530

    Created google login API

--- router/api.js
+++ router/api.js
@@ -5,11 +5,13 @@ const router = Router();
 const controllerLogin = require("../controllers/login.js");
 const controllerRegister = require("../controllers/register.js");
 const controllerChat = require("../controllers/chat.js"); 
+const controllerGoogleLogin = require('../controllers/googleLogin.js');
 
 /** POST Methods */
 router.route("/login").post(controllerLogin.login); // login in app
 router.route("/register").post(controllerRegister.register); // registration in app
 router.route("/chat").post(controllerChat.sendChat);
+router.route('/google_login').post(controllerGoogleLogin.googleLogin);
 
 /** GET Methods */
 router.route("/chat").get(controllerChat.chatHistory);
