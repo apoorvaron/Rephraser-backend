70c7a4c185722155ff9a7a37f4518ac83b5c4bd1
Nidhi Rathore <nidhirathore7022@gmail.com>
2023-09-20 13:14:40 +0530

    Update Prompt for appreciated words

--- utils/openaiUtils.js
+++ utils/openaiUtils.js
@@ -1,6 +1,6 @@
 // utils/openaiUtils.js
 const { OpenAI } = require("openai");
-const SYSTEM_PROMPT = "If someone asks a question like write something or tell or ask or suggest or give commands you to write something either in any type of language or in whatsapp language, just convert the question in correct grammatical english and if the given statement is already in correct grammatical english then do not change the statement. In case someone asks you to convert a statement in any language then convert the statement into english only and remove the language name";
+const SYSTEM_PROMPT = "If someone asks a question like write something or tell or ask or suggest or writes appreciation/Compliment statement or give commands you to write something either in any type of language or in whatsapp language, just convert the question in correct grammatical english and if the given statement is already in correct grammatical english then do not change the statement. In case someone asks you to convert a statement in any language then convert the statement into english only and remove the language name.";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
